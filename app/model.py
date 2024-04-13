b19811143edee9d34003bab275f322c49ffe1383
Serj Smor <serjsmor@gmail.com>
2024-04-13 09:02:12 +0300

    Small refactors
    Classification report per dataset, enable raw predictions (without system prompt),

--- app/model.py
+++ app/model.py
@@ -13,15 +13,21 @@ class IntentClassifier:
         input_text = build_prompt(text, prompt_options, company_name, company_portion)
         # print(input_text)
         # Tokenize the concatenated inp_ut text
-        input_ids = self.tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True).to(self.device)
-
-        # Generate the output
-        output = self.model.generate(input_ids)
-
-        # Decode the output tokens
-        decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        decoded_output = self._predict(input_text)
 
         return decoded_output
+
+    def _predict(self, input_text):
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True).to(
+            self.device)
+        # Generate the output
+        output = self.model.generate(input_ids)
+        # Decode the output tokens
+        decoded_output = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return decoded_output
+
+    def raw_predict(self, text) -> str:
+        return self._predict(text)
 
 if __name__ == '__main__':
     m = IntentClassifier("serj/intent-classifier")
--- results.py
+++ results.py
@@ -8,10 +8,12 @@ from consts import DEFAULT_PREDICTION_CSV
 
 def extract_class_name(prediction_text: str) -> str:
     prediction_text = prediction_text.lower()
+    class_name = prediction_text
     print(prediction_text)
-    class_name = prediction_text.split(":")[1]
     if ":" in prediction_text:
         class_name = prediction_text.split(":")[1]
+        if ":" in prediction_text:
+            class_name = prediction_text.split(":")[1]
 
     return class_name.strip()
 
@@ -31,24 +33,34 @@ def is_generator_label_in_prompt_options(row):
 
     return False
 
-def results(csv_path):
+def results(csv_path, per_dataset):
     predictions_df = pd.read_csv(csv_path)
-    print(predictions_df["prediction"].value_counts())
-    predictions_df["class_name"] = predictions_df["class_name"].apply(str.lower)
-    predictions_df["generator_label_prediction"] = predictions_df["prediction"].apply(extract_class_name)
-    # count how many predictions are actually in the taxonomy 1X1
-    predictions_df["is_generator_label_in_prompt_options"] = predictions_df.apply(
-        lambda row: is_generator_label_in_prompt_options(row), axis=1)
-    print(predictions_df["is_generator_label_in_prompt_options"].value_counts())
-    missing_class_name = predictions_df[predictions_df['is_generator_label_in_prompt_options'] == False].shape[0]
-    print(f"{missing_class_name / predictions_df.shape[0]}")
-    good_predictions_df = predictions_df[predictions_df["is_generator_label_in_prompt_options"] == True]
+    if per_dataset:
+        datasets = predictions_df["dataset_name"].unique().tolist()
+        for dataset in datasets:
+            df = predictions_df[predictions_df["dataset_name"] == dataset]
+            calculate_classification_report(df)
+    else:
+        calculate_classification_report(predictions_df)
 
-    print(classification_report(good_predictions_df["class_name"], good_predictions_df["generator_label_prediction"]))
+def calculate_classification_report(df):
+    print(df["prediction"].value_counts())
+    df["class_name"] = df["class_name"].apply(str.lower)
+    df["generator_label_prediction"] = df["prediction"].apply(extract_class_name)
+    # count how many predictions are actually in the taxonomy 1X1
+    df["is_generator_label_in_prompt_options"] = df.apply(
+        lambda row: is_generator_label_in_prompt_options(row), axis=1)
+    print(df["is_generator_label_in_prompt_options"].value_counts())
+    missing_class_name = df[df['is_generator_label_in_prompt_options'] == False].shape[0]
+    print(f"{missing_class_name / df.shape[0]}")
+    good_df = df[df["is_generator_label_in_prompt_options"] == True]
+    print(classification_report(good_df["class_name"], good_df["generator_label_prediction"]))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Results")
     parser.add_argument("-p", "--csv-path", default=DEFAULT_PREDICTION_CSV)
+    parser.add_argument("-pd", "--per-dataset", action="store_true")
     args = parser.parse_args()
 
-    results(args.csv_path)
+    results(args.csv_path, args.per_dataset)
