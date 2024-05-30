b5efc749f6a4bcb5ff891f4a5daa2d17a04a8ad5
Serj Smor <serjsmor@gmail.com>
2024-05-30 08:46:05 +0300

    Add the Atis pipeline

--- tasks.py
+++ tasks.py
@@ -1,24 +1,125 @@
-from invoke import task
+import json
+from dataclasses import dataclass
+
+import pandas as pd
+from datasets import load_dataset
+from invoke import task, Context
 from transformers import pipeline, T5ForConditionalGeneration, T5Tokenizer
+from tqdm import tqdm
 
+# app
+from dataset import load_main_dataset
+from preprocess import preprocess_simple_dataset, preprocess
 from app.model import IntentClassifier
-from preprocess import preprocess_simple_dataset
+from app.utils import get_model_suffix
+from app.atis.utils import ATIS_INTENT_MAPPING as intent_mapping
+
+
+FLAN_T_BASE = "google/flan-t5-base"
+
+@dataclass
+class Summary:
+    batch_size: int
 
 @task
-def preprocess(c):
+def preprocess_dataset(c):
     c.run("python preprocess.py")
 
 @task
-def train(c, model_name="google/flan-t5-base", epochs=6, batch_size=8):
-    c.run("python preprocess.py")
+def train(c, is_preprocess=False, model_name=FLAN_T_BASE, epochs=6, batch_size=8, per_dataset=False):
+    if is_preprocess:
+        c.run("python preprocess.py")
+
     c.run(f"python t5_generator_trainer.py --model-name {model_name} -e {epochs} -bs {batch_size}")
     model_suffix = model_name.split("/")[1]
     c.run(f"python predict.py --model-name models/{model_suffix} -p data/test.csv")
-    c.run("python results.py -p data/predictions_test.csv -pd")
+    per_dataset_str = "" if not per_dataset else " -pd "
+    c.run(f"python results.py -p results/predictions_test.csv {per_dataset_str}")
+
 
 @task
-def train_base(c):
-    train(c, model_name="google/flan-t5-base")
+def train_atis_zero_shot(c, model_name=FLAN_T_BASE, epochs=15, batch_size=8):
+    dataset = load_main_dataset()
+    dataset_to_remove = "Atis Arilines"
+    # remove atis
+    filtered_dataset = [var for var in dataset if var["Company Name"] != dataset_to_remove]
+
+    preprocess(filtered_dataset)
+    train(c, is_preprocess=False, model_name=model_name, epochs=epochs, batch_size=batch_size)
+
+
+@task
+def test_atis(c, model_name=FLAN_T_BASE):
+    # atis test set
+    model_suffix = get_model_suffix(model_name)
+    full_model_path = f"models/{model_suffix}"
+    model = IntentClassifier(model_name=full_model_path)
+
+    dataset = load_dataset("tuetschek/atis")
+    intents = set([row["intent"] for row in dataset["test"]])
+
+    prompt_options = "OPTIONS\n"
+    index = 1
+
+    for intent in intents:
+        if intent not in intent_mapping:
+            continue
+
+        mapping = intent_mapping[intent]
+        prompt_options += f" {index}. {mapping} \n"
+        index += 1
+    prompt_options = f'''{prompt_options}'''
+
+    results = []
+    company_name = "Atis Airlines"
+    company_specific = "Airlines flights, meals, seats customer requests"
+
+    for row in tqdm(dataset["test"]):
+        intent = row["intent"]
+        if intent not in intent_mapping:
+            continue
+
+        prediction = model.predict(row["text"], prompt_options, company_name, company_specific)
+        # keywords = model.raw_predict(f"All of the verbs: {row['text']}")
+        results.append({"prediction": prediction, "y": intent_mapping[intent], "text": row["text"]})
+
+    from sklearn.metrics import classification_report
+    y = [r["y"] for r in results]
+    predictions = [r["prediction"].replace("Class name: ", "") for r in results]
+    classification_report_df = pd.DataFrame(classification_report(y, predictions, output_dict=True)).T
+    print(classification_report_df)
+
+    classification_report_df.to_csv("results/classification_report.csv", index=False)
+
+
+@task
+def atis_pipeline(c, name="", model_name=FLAN_T_BASE, epochs=15, batch_size=8):
+    train_atis_zero_shot(c, model_name, epochs, batch_size)
+    if len(name) == 0:
+        model_suffix = get_model_suffix(model_name)
+        name = f"{model_suffix}_e{epochs}_b{batch_size}"
+    pipeline_args = {"model name": model_name, "epochs": epochs, "batch size": batch_size}
+    with open("data/train_args.txt", "w") as f:
+        json.dump(pipeline_args, f)
+
+    test_atis(c, model_name)
+    archive(c, name)
+
+@task
+def archive(c, name):
+    experiment_path = f"archived_experiments/{name}"
+    c.run(f"mkdir {experiment_path}")
+    c.run(f"cp -R data {experiment_path}/")
+    c.run(f"cp -R models {experiment_path}/")
+    c.run(f"cp -R results {experiment_path}/")
+
+
+
+
+
+
+
+
 
 @task
 def train_small(c):
@@ -49,7 +150,7 @@ def predict_test_hf_model(c):
 def upload_to_hf(c):
     model = T5ForConditionalGeneration.from_pretrained("models/flan-t5-base")
     # push to the hub
-    model.push_to_hub("intent-classifier", token="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", commit_message="Add Atis dataset sub samples to training dataset")
+    model.push_to_hub("intent-classifier", token="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", commit_message="Add clinc_oos dataset sub samples to training dataset")
 
 @task
 def upload_to_hf_small(c):
@@ -57,3 +158,10 @@ def upload_to_hf_small(c):
     # push to the hub
     model.push_to_hub("intent-classifier-flan-t5-small", token="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",
                       commit_message="Add Atis dataset sub samples to training dataset")
+
+
+
+
+
+if __name__ == '__main__':
+    train_atis_zero_shot(Context())
