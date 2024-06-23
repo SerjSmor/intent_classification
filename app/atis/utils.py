fcdcab5e10ea3a9d20153debf47bdc3db8b2eb70
Serj Smor <serjsmor@gmail.com>
2024-06-23 23:52:10 +0300

    Add cmd parameters for atis testing to

--- app/atis/utils.py
+++ app/atis/utils.py
@@ -1,3 +1,6 @@
+import argparse
+
+import numpy as np
 import pandas as pd
 import torch
 from datasets import load_dataset
@@ -6,13 +9,13 @@ from sklearn.metrics import classification_report
 
 from app.model import IntentClassifier
 from app.utils import get_model_suffix, build_prompt
-from consts import ATIS_TEST_SET_CLASSIFICATION_REPORT_CSV
+from consts import ATIS_TEST_SET_CLASSIFICATION_REPORT_CSV, ATIS_PREDICTIONS_CSV, FLAN_T5_SMALL, LOCAL_FLAN_T5_SMALL
 
 ATIS_INTENT_MAPPING = {
     'abbreviation': "Abbreviation and Fare Code Meaning Inquiry",
     'aircraft': "Aircraft Type Inquiry",
     # 'aircraft+flight+flight_no': "Flight Details Inquiry",
-    'airfare': "Airfare Information Requests",
+    'airfare': "Airfare Information Request",
     # 'airfare+flight_time': "Flight Cost and Duration Inquiry",
     'airline': "Airline Information Request",
     # 'airline+flight_no': "Airline and Flight Number Query",
@@ -33,14 +36,16 @@ ATIS_INTENT_MAPPING = {
     'restriction': "Flight Restriction Inquiry"
 }
 
-def test_atis_dataset(full_model_path, intent_mapping=ATIS_INTENT_MAPPING, no_company_specific=False, no_number_prompt=False):
+def test_atis_dataset(full_model_path, intent_mapping=ATIS_INTENT_MAPPING, save_file=True, use_default_labels=False,
+                      no_company_specific=True, no_number_prompt=False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = IntentClassifier(model_name=full_model_path, device=device)
 
     dataset = load_dataset("tuetschek/atis")
-    intents = set([row["intent"] for row in dataset["test"]])
+    all_intents = [row["intent"] for row in dataset["test"]]
+    intents = np.unique(all_intents)
 
-    prompt_options = create_prompt_options(intent_mapping, intents, no_number_prompt)
+    prompt_options = create_prompt_options(intent_mapping, intents, no_number_prompt, use_default_labels)
 
     results = []
     company_name = "Atis Airlines"
@@ -55,37 +60,68 @@ def test_atis_dataset(full_model_path, intent_mapping=ATIS_INTENT_MAPPING, no_co
         prediction = model.raw_predict(input_text)
         prediction = prediction.replace("Class name: ", "")
         # keywords = model.raw_predict(f"All of the verbs: {row['text']}")
-        results.append({"prediction": prediction, "y": intent_mapping[intent], "text": row["text"], "prompt": input_text,
+        y = intent_mapping[intent]
+        if use_default_labels:
+            y = row["intent"]
+
+        results.append({"prediction": prediction, "y": y, "text": row["text"], "prompt": input_text,
                         "prompt_options": prompt_options})
 
+
     df_results = pd.DataFrame(results)
-    df_results.to_csv("results/atis_predictions.csv", index=False)
+    print(f"## printing atis predictions distribution for model: {full_model_path}")
+    print(df_results["prediction"].value_counts())
+
+    if save_file:
+        df_results.to_csv(ATIS_PREDICTIONS_CSV, index=False)
     df_errors = df_results[df_results["y"] != df_results["prediction"]]
-    df_errors.to_csv("results/atis_errors.csv", index=False)
+    if save_file:
+        df_errors.to_csv("results/atis_errors.csv", index=False)
     y = [r["y"] for r in results]
     predictions = [r["prediction"].replace("Class name: ", "") for r in results]
-
-    print(classification_report(y, predictions, output_dict=True))
+    print(predictions[:10])
+    print(y[:10])
+    print(classification_report(y, predictions))
     classification_report_df = pd.DataFrame(classification_report(y, predictions, output_dict=True)).T
     classification_report_df["id"] = classification_report_df.index.tolist()
 
-    classification_report_df.to_csv(ATIS_TEST_SET_CLASSIFICATION_REPORT_CSV, index=False)
+    if save_file:
+        classification_report_df.to_csv(ATIS_TEST_SET_CLASSIFICATION_REPORT_CSV, index=False)
+
     return classification_report_df
 
 
-def create_prompt_options(intent_mapping, intents, no_number_prompt=False) -> str:
+def create_prompt_options(intent_mapping, intents, no_number_prompt=False, use_default_labels=False) -> str:
+    print(f"create prompt options: intent_mapping: {intent_mapping}, no_number_prompt: {no_number_prompt},"
+          f" use_default_labels: {use_default_labels}")
     prompt_options = "OPTIONS\n"
     index = 1
     for intent in intents:
         if intent not in intent_mapping:
             continue
 
-        mapping = intent_mapping[intent]
+        class_name = intent_mapping[intent]
+        if use_default_labels:
+            class_name = intent
         if no_number_prompt:
-            prompt_options += f"% {mapping} \n"
+            prompt_options += f"# {class_name} \n"
         else:
-            prompt_options += f" {index}. {mapping} \n"
+            prompt_options += f" {index}. {class_name} \n"
 
         index += 1
     prompt_options = f'''{prompt_options}'''
+    print(f"prompt options: {prompt_options}")
     return prompt_options
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Predict atis")
+    parser.add_argument("-m", "--model-name", default=LOCAL_FLAN_T5_SMALL)
+    parser.add_argument("--use-default-labels", action="store_true")
+    parser.add_argument("--no-number-prompt", action="store_true")
+    args = parser.parse_args()
+
+    print(vars(args))
+
+    test_atis_dataset(args.model_name, save_file=True, use_default_labels=args.use_default_labels, no_number_prompt=args.no_number_prompt,
+                      no_company_specific=True)
+
