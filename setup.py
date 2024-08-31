65dabfa3b19885192f90a9d1785977b867c92e28
Serj Smor <serjsmor@gmail.com>
2024-08-31 09:36:00 +0300

    Add a minimal setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="foo",
+    version="0.0.1",
+    packages=["app"],
+    description="This library enables to mix and match classification components for prompt based classifier."
+)
